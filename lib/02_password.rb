5c550a3c6ae1f40eff0a33fdc0e0f2ff9b88859f
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-05 22:16:42 +0200

    password exo solved

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -8,18 +8,24 @@ def login(password)
     puts "Please enter your password below :"
     attempt = gets.chomp
     while (attempt != password) do
-        puts "WRONG"
+        puts "THIS IS NOT THE PASSWORD, TRY AGAIN BEFORE SELF-DESTRUCTION"
         attempt = gets.chomp
+    end
+    if attempt == password
+        welcome_screen()
     end
 end
 
 def welcome_screen()
-
+    puts "Welcome to your secret numeric platform, special agent !"
+    puts "The last secret information we've got is that THP holds many motivated mates ..."
+    puts "Now you are logged off for safety and your account is self-destruct"
 end
 
 def perform()
     password = signup()
     login(password)
+
 end
 
 perform()
