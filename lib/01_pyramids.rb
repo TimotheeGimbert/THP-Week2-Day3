237c613fc35576de77a517f6781d5093e914ca09
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-05 21:48:23 +0200

    pyramid losange Solved

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,5 +1,4 @@
 def getInput()
-  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
   print "> "
   input = gets.chomp.to_i
   while (Integer(input) === false || input <= 0 || input >= 26) do
@@ -10,12 +9,12 @@ def getInput()
   return input
 end
   
-def drawHalfPyramid(n)
-  for i in 1..n do # FOR each line n
-    for j in 1..(n-i) do # FOR each free spot on the line n
+def draw_half_pyramid(n)
+  for i in 1..n do
+    for j in 1..(n-i) do
       print " "
     end
-    for j in (n-i + 1)..n do # FOR each drawing spot on the line n
+    for j in (n-i + 1)..n do
       print "#"
     end
     puts
@@ -23,15 +22,16 @@ def drawHalfPyramid(n)
 end
   
 def half_pyramid()
+  puts "Salut, bienvenue dans ma demi pyramide ! Combien d'étages veux-tu ?"
   nbFloors = getInput()
-  puts "Voici une pyramide à #{nbFloors} étages :"
-  drawHalfPyramid(nbFloors)
+  puts "Voici une demi-pyramide à #{nbFloors} étages :"
+  draw_half_pyramid(nbFloors)
 end
 
-def drawFullPyramid(n)
+def draw_full_pyramid(n)
   for i in 1..n do
     nbSpaces = n - i
-    for j in 0..nbSpaces do
+    for j in 1..nbSpaces do
         print " "
     end
     nbPoints = 1 + 2 * (i -1)
@@ -43,9 +43,43 @@ def drawFullPyramid(n)
 end
 
 def full_pyramid()
+  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
   nbFloors = getInput()
   puts "Voici une pyramide à #{nbFloors} étages :"
-  drawFullPyramid(nbFloors)
+  draw_full_pyramid(nbFloors)
 end
 
-full_pyramid()
+def addBottom_full_pyramid(n)
+  for l in 1..n do
+    nbSpaces = l 
+    nbSpaces.times do
+      print " "   
+    end
+    nbPoints = (n*2) - (2*l) + 1
+    nbPoints.times do
+      print "#"
+    end
+    puts
+  end
+end
+
+def wtf_pyramid()
+  puts "Salut, bienvenue dans ma super pyramide losange ! Combien d'étages veux-tu ?"
+  nbFloors = getInput()
+  while (nbFloors.even? === true) do
+    puts "It cannot be an even number ... !"
+    nbFloors = getInput()
+  end
+  puts "Voici la pyramide losange à #{nbFloors} étages :"
+  draw_full_pyramid((nbFloors+1)/2)
+  addBottom_full_pyramid(nbFloors/2)
+end
+
+
+def perform()
+  half_pyramid()
+  full_pyramid()
+  wtf_pyramid()
+end
+
+perform()
