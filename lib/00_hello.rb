cae0c483b4ff41baa2ea6ff18139ca2b796ae568
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-05 18:23:44 +0200

    Adds resolved 00-hello

---
+++ lib/00_hello.rb
@@ -0,0 +1,12 @@
+def say_hello(first_name)
+    puts "Bonjour, #{first_name} !"
+end
+
+def ask_first_name()
+    puts "Comment t'appelles-tu ?"
+    first_name = gets.chomp
+    return first_name
+end
+
+first_name = ask_first_name()
+say_hello(first_name)
